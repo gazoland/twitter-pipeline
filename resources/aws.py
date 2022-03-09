845b2922bb77a2069782fd20a5a4c6c592fb8845
Giuliano <giuliano.gs@outlook.com>
2022-03-09 18:09:44 -0300

    Added: read_s3_file function; Updated: upsert_batch function

--- resources/aws.py
+++ resources/aws.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import json
 
 ACCESS_KEY_ID = os.environ.get("S3_ACCESS_KEY_ID")
 SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
@@ -11,3 +12,13 @@ def upload_to_s3(filename, path):
                             region_name="us-east-1")
     s3 = session.resource("s3")
     s3.meta.client.upload_file(os.path.join(os.getcwd(), filename), S3_BUCKET, path + filename)
+
+
+def read_s3_file(file_key):
+    session = boto3.Session(aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY,
+                            region_name="us-east-1")
+    s3 = session.resource("s3")
+    obj = s3.Object(S3_BUCKET, file_key)
+    data = obj.get()["Body"].read().decode('utf-8')
+    json_data = json.loads(data)
+    return json_data
--- resources/database.py
+++ resources/database.py
@@ -23,48 +23,40 @@ def connect_to_database():
     return conn_object
 
 
-def upsert(conn, df, table, conflict_column):
-    con = psycopg2.connect(
-        host='',
-        user='',
-        password='',
-        dbname='',
-        port=0
-    )
+def upsert_batch(conn, dataframe, schema, table, conflict: bool, conflict_column=None):
 
-    cur = con.cursor()
-    cols = ','.join(list(df))
-    vals = ','.join(['%s'] * len(list(df)))
-    updates = ','.join([x + '=%s' for x in list(df)[1:]])
-    rows = [list(x) for x in df.to_numpy()]
-    qry = f"insert into {table} ({cols}) values ({vals}) on conflict ({conflict_column}) do update set {updates}"
-    time.sleep(0.2)
-    for i in tqdm(rows):
+    cols = ','.join(list(dataframe.columns))
+    rows = [list(x) for x in dataframe.to_numpy()]
+    vals = '%s,' * (len(rows[0]) - 1) + '%s'
+    for i in rows:
         i.extend(i[1:])
-        j = [None if pd.isna(x) else x for x in i]
-        k = [int(y) if type(y) == np.int64 else y for y in j]
-        try:
-            cur.execute(qry, tuple(k))
-        except psycopg2.ProgrammingError as pgerr:
-            print(updates)
-            for x in k:
-                print(x, type(x), pgerr)
-                con.close()
-                input('dsg')
-        con.commit()
-    con.close()
-    print('Done.')
+    tuples = [tuple(y) for y in rows]
+    updates = ','.join([x + '=%s' for x in list(dataframe)[1:]])
+
+    if conflict:
+        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES ({vals})" \
+                f" ON CONFLICT ({conflict_column}) DO UPDATE SET {updates}"
+    else:
+        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES ({vals})"
+
+    cursor = conn.cursor()
+    psycopg2.extras.execute_batch(cursor, query, tuples)
+    conn.commit()
+    cursor.close()
+    # conn.close()
+    print('Done')
 
 
 def insert_batch(conn, dataframe, schema, table, conflict: bool, conflict_column=None):
 
-    tuples = [tuple(x) for x in dataframe.to_numpy()]
     cols = ','.join(list(dataframe.columns))
-    aux = '(' + '%s,' * (len(tuples[0]) - 1) + '%s)'
+    tuples = [tuple(x) for x in dataframe.to_numpy()]
+    vals = '%s,' * (len(tuples[0]) - 1) + '%s'
+
     if conflict:
-        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES {aux} ON CONFLICT ({conflict_column}) DO NOTHING"
+        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES ({vals}) ON CONFLICT ({conflict_column}) DO NOTHING"
     else:
-        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES {aux}"
+        query = f"INSERT INTO {schema}.{table} ({cols}) VALUES ({vals})"
 
     cursor = conn.cursor()
     psycopg2.extras.execute_batch(cursor, query, tuples)
