7fa6c6474be5671f4872c2b5495b5c91e6314399
Giuliano <giuliano.gs@outlook.com>
2022-04-15 22:59:16 -0300

    added bearer token authentication

--- api/controllers/users.py
+++ api/controllers/users.py
@@ -1,12 +1,24 @@
+import os
 from flask_restx import Resource, reqparse, abort
 from api import api_resources
+from flask_httpauth import HTTPTokenAuth
+
+auth = HTTPTokenAuth()
+API_TOKEN = os.environ.get("API_TOKEN")
+
+
+@auth.verify_token
+def verify_token(token):
+    return token == API_TOKEN
 
 
 class Users(Resource):
+    @auth.login_required
     def get(self):
         usernames = get_usernames(filename="../src/usernames.txt")
         return {"data": {"usernames": usernames}}
 
+    @auth.login_required
     def post(self):
         users_post_args = reqparse.RequestParser()
         users_post_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form", required=True)
@@ -26,6 +38,7 @@ class Users(Resource):
 
         # Add tasks with AWS Lambda functions
 
+    @auth.login_required
     def delete(self):
         users_delete_args = reqparse.RequestParser()
         users_delete_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form", required=True)
@@ -67,4 +80,4 @@ def remove_usernames(*args, current_filename, new_filename):
 
 
 def abort_post_existing_users(*args):
-    return abort(400, message=f"Error. These users are already listed: {', '.join(args)}")
+    return abort(409, message=f"Error. These users are already listed: {', '.join(args)}")
