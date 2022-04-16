8aeb392db688e3bc9a9772173ebabf22c92c3e49
Giuliano <giuliano.gs@outlook.com>
2022-04-15 22:59:40 -0300

    added headers for authentication

--- api/request_test.py
+++ api/request_test.py
@@ -1,24 +1,31 @@
+import os
+
 import requests
 import json
 
 BASE = "http://127.0.0.1:5000/v1/"
 # headers={"Content-type": "application/json"},
+API_TOKEN = os.environ.get("API_TOKEN")
+headers = {"Accept": "application/json",
+           "Authorization": f"Bearer {API_TOKEN}"}
 
 
 def test_get(resource):
-    resp = requests.request("GET", url=BASE + resource)
+    resp = requests.request("GET", url=BASE + resource, headers=headers)
     print(resp.json())
+    print(resp)
 
 
 def test_post(resource):
-    resp = requests.request("POST", url=BASE + resource, data={"usernames": "joe,neil,john,liam"})
-    # resp = requests.post(BASE+resource, data={"username": "john"})
+    resp = requests.request("POST", url=BASE + resource, headers=headers, data={"usernames": "joe,neil,john,liam"})
     print(resp.json())
+    print(resp)
 
 
 def test_delete(resource):
-    resp = requests.request("DELETE", url=BASE + resource, data={"usernames": "john,liam,joe,neil"})
+    resp = requests.request("DELETE", url=BASE + resource, headers=headers, data={"usernames": "john,liam,joe,neil"})
     print(resp.json())
+    print(resp)
 
 
 if __name__ == "__main__":
