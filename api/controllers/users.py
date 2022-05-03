b08c3ee6b2256d5969a3bd0c12a40ee8b6f5e3b2
Giuliano <giuliano.gs@outlook.com>
2022-05-03 12:05:39 -0300

    added database integrations to the GET POST and DELETE methods

--- api/controllers/users.py
+++ api/controllers/users.py
@@ -16,22 +16,23 @@ def verify_token(token):
 class Users(Resource):
     @auth.login_required
     def get(self):
-        usernames = get_usernames(filename=usernames_file)
+        usernames = get_usernames()
         return {"data": {"usernames": usernames}}
 
     @auth.login_required
     def post(self):
         users_post_args = reqparse.RequestParser()
-        users_post_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form", required=True)
+        users_post_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form",
+                                     required=True)
         args = users_post_args.parse_args()
 
         # ERRORS:
         # ''; whitespace
         passed_users = args["usernames"].split(",")
-        all_new, existing_users = check_usernames(*passed_users, filename=usernames_file)
+        all_new, existing_users = check_usernames(*passed_users)
 
         if all(all_new):
-            add_usernames(*passed_users, current_filename=usernames_file, new_filename=usernames_file)
+            add_usernames(*passed_users)
             return f"Added usernames: {', '.join(passed_users)}", 201
 
         else:
@@ -42,42 +43,57 @@ class Users(Resource):
     @auth.login_required
     def delete(self):
         users_delete_args = reqparse.RequestParser()
-        users_delete_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form", required=True)
+        users_delete_args.add_argument("usernames", type=str, help="Twitter usernames required", location="form",
+                                       required=True)
         args = users_delete_args.parse_args()
 
         passed_users = args["usernames"].split(",")
-        updated, deleted, not_found = remove_usernames(*passed_users, current_filename=usernames_file, new_filename=usernames_file)
-        deleted_message = f"Users deleted from list: {', '.join(deleted)}" if len(deleted) > 0 else ""
-        not_found_message = f"Users not found in list: {', '.join(not_found)}" if len(not_found) > 0 else ""
+        deleted, not_found = remove_usernames(*passed_users)
+        deleted_message = f"Users deleted from database: {', '.join(deleted)}" if len(deleted) > 0 else ""
+        not_found_message = f"Users not found in database: {', '.join(not_found)}" if len(not_found) > 0 else ""
         return f"{deleted_message}\n{not_found_message}"
 
 
-def get_usernames(filename):
-    with open(filename) as file:
-        all_usernames = file.read().rstrip("\n").split(",")
+def get_usernames():
+    db_conn = api_resources.connect_to_database()
+    cur = db_conn.cursor()
+    cur.execute("SELECT username FROM twitter.usernames")
+    all_usernames = [u[0] for u in cur.fetchall()]
+    db_conn.close()
     return all_usernames
 
 
-def check_usernames(*args, filename):
-    usernames = get_usernames(filename=filename)
+def check_usernames(*args):
+    usernames = get_usernames()
     new_users = [u not in usernames for u in args]
     duplicated_users = [u for u in args if u in usernames]
     return new_users, duplicated_users
 
 
-def add_usernames(*args, current_filename, new_filename):
-    users_list = get_usernames(filename=current_filename)
-    users_list.extend(args)
-    api_resources.write_text_file(users_list, filename=new_filename)
+def add_usernames(*args):
+    print(args)
+    db_conn = api_resources.connect_to_database()
+    api_resources.insert_batch(
+        conn=db_conn,
+        multicolumn=False,
+        data=args,
+        schema="twitter",
+        table="usernames",
+        conflict=False,
+        single_column="username")
+    db_conn.close()
 
 
-def remove_usernames(*args, current_filename, new_filename):
-    usernames = get_usernames(filename=current_filename)
-    updated_users = [u for u in usernames if u not in args]
-    deleted_users = [u for u in usernames if u in args]
+def remove_usernames(*args):
+    usernames = get_usernames()
+    users_to_delete = [u for u in usernames if u in args]
     not_found_users = [u for u in args if u not in usernames]
-    api_resources.write_text_file(updated_users, filename=new_filename)
-    return updated_users, deleted_users, not_found_users
+    db_conn = api_resources.connect_to_database()
+    cur = db_conn.cursor()
+    cur.execute("DELETE FROM twitter.usernames WHERE username in ('{}')".format("','".join(users_to_delete)))
+    db_conn.commit()
+    db_conn.close()
+    return users_to_delete, not_found_users
 
 
 def abort_post_existing_users(*args):
